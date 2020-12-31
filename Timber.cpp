657954d6da605f2971f6ca76bb3ad8114d05a1eb
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-30 23:28:09 -0500

    draw bg sprite

--- Timber.cpp
+++ Timber.cpp
@@ -9,6 +9,21 @@ int main()
     // create and open a window for the game 
     sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
 
+    // create a texture to hold a graphic on the GPU
+    sf::Texture textureBackground;
+
+    // load a graphic into the texture 
+    textureBackground.loadFromFile("graphics/background.png");
+
+    // create a sprite 
+    sf::Sprite spriteBackground;
+
+    // attach the texture to the sprite 
+    spriteBackground.setTexture(textureBackground);
+
+    // set the spriteBackground to cover the screen
+    spriteBackground.setPosition(0, 0);
+
     while (window.isOpen())
     {
         /** Handle Player Input */
@@ -22,7 +37,7 @@ int main()
         window.clear();
 
         // Draw
-        // TODO
+        window.draw(spriteBackground);
 
         // Show everything we just drew
         window.display();
