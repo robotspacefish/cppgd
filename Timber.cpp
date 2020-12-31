20a530f49e0f6e1702ac3c67005d06bc50e8b1d3
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-12-30 23:10:07 -0500

    include SFML Graphics and setup window

--- Timber.cpp
+++ Timber.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+    // create a video mode object
+    sf::VideoMode vm(1920, 1080);
+
+    // create and open a window for the game 
+    sf::RenderWindow window(vm, "Timber!!!", sf::Style::Fullscreen);
+
     return 0;
 }
