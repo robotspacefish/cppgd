a7835ca508cdb5032083417b93ed4190234bc2b5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-08 00:24:23 -0500

    add global constants

--- tictactoe/tictactoe/main.cpp
+++ tictactoe/tictactoe/main.cpp
@@ -2,6 +2,14 @@
 #include <string>
 #include <vector>
 
+// global constants
+const char X = 'X';
+const char O = 'O';
+const char EMPTY = ' ';
+const char TIE = 'T';
+const char NO_ONE = 'N';
+
 int main() {
+    
     return 0;
 }
