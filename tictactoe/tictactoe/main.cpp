f26cde7d3a727d7e2a6ca586d4fca9ad0d6ac6b6
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-08 00:33:31 -0500

    display instructions

--- tictactoe/tictactoe/main.cpp
+++ tictactoe/tictactoe/main.cpp
@@ -9,7 +9,26 @@ const char EMPTY = ' ';
 const char TIE = 'T';
 const char NO_ONE = 'N';
 
+
+void instructions();
+
 int main() {
-    
+   
+    instructions();
     return 0;
+}
+
+void instructions()
+{
+    using namespace std;
+    
+    cout << "Welcome to Tic-Tac-Toe!" << "\n";
+    cout << "0 - 8 represent spaces on the board. Enter the number\n";
+    cout << "corresponding to the position you want to make your mark in.\n\n";
+    
+    cout << " 0 | 1 | 2\n";
+    cout << "-----------\n";
+    cout << " 3 | 4 | 5\n";
+    cout << "-----------\n";
+    cout << " 6 | 7 | 8\n\n";
 }
