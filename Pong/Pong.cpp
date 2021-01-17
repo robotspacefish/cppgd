70ecee62870c949e5868816fe75713b19088da23
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-16 21:31:57 -0500

    create video mode object and render window

--- Pong/Pong.cpp
+++ Pong/Pong.cpp
@@ -5,5 +5,11 @@
 
 int main()
 {
+	// create a video mode object 
+	sf::VideoMode vm(1920, 1080);
+
+	// create and open a window for the game 
+	sf::RenderWindow window(vm, "Pong", sf::Style::Fullscreen);
+
 	return 0;
 }
