6b9c15aee0ab4a9d3a64d39da8316493471a6d9f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-01-24 23:02:27 -0500

    randomize floor textures & place walls

--- ZombieArena/ZombieArena.cpp
+++ ZombieArena/ZombieArena.cpp
@@ -28,6 +28,29 @@ int createBackground(sf::VertexArray& rVA, sf::IntRect arena)
 			rVA[currentVertex + 2].position = sf::Vector2f(w * TILE_SIZE, h * TILE_SIZE);
 			rVA[currentVertex + 3].position = sf::Vector2f(w * TILE_SIZE, h * TILE_SIZE);
 
+			// define the position in the Texture for current quad
+			// either grass, stone, bush, or wall
+			if (h == 0 || h == worldHeight - 1 || w == 0 || w == worldWidth - 1)
+			{
+				// wall texture
+				rVA[currentVertex].texCoords = sf::Vector2f(0, TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 1].texCoords = sf::Vector2f(TILE_SIZE, TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 2].texCoords = sf::Vector2f(TILE_SIZE, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+				rVA[currentVertex + 3].texCoords = sf::Vector2f(0, TILE_SIZE + TILE_TYPES * TILE_SIZE);
+			}
+			else
+			{
+				// use a random floor texture
+				srand((int)time(0) + h * w - h);
+				int mOrG = (rand() % TILE_TYPES); // mud or grass
+				int verticalOffset = mOrG * TILE_SIZE;
+
+				rVA[currentVertex].texCoords = sf::Vector2f(0, verticalOffset);
+				rVA[currentVertex + 1].texCoords = sf::Vector2f(TILE_SIZE, verticalOffset);
+				rVA[currentVertex + 2].texCoords = sf::Vector2f(TILE_SIZE, TILE_SIZE + verticalOffset);
+				rVA[currentVertex + 3].texCoords = sf::Vector2f(0, TILE_SIZE + verticalOffset);
+			}
+
 			// position ready for the next 4 vertices
 			currentVertex += VERTS_IN_QUAD;
 		}
